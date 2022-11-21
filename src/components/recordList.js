bd1088942a58595a9c89c40bb8d430aa181cec1c
Christopher DiStasio <cadistasio@gmail.com>
2022-11-20 20:41:57 -0600

    Updates dependencies; removes cors-anywhere

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -25,7 +25,7 @@ export default function RecordList() {
  // This method fetches the records from the database.
  useEffect(() => {
    async function getRecords() {
-     const response = await fetch(`https://cors-anywhere.herokuapp.com/https://sample-mern-frontend.vercel.app/record/`);
+     const response = await fetch(`https://sample-mern-frontend.vercel.app/record/`);
 
      if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
@@ -44,7 +44,7 @@ export default function RecordList() {
 
  // This method will delete a record
  async function deleteRecord(id) {
-   await fetch(`https://cors-anywhere.herokuapp.com/https://sample-mern-frontend.vercel.app/${id}`, {
+   await fetch(`https://sample-mern-frontend.vercel.app/${id}`, {
      method: "DELETE"
    });
 
